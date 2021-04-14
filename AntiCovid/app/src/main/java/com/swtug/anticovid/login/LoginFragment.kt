f98d20adcaf15eaa0221cc40c9532c09880c15b9
Lukas Brugger <lukas.brugger404@gmail.com>
2021-04-14 16:51:18 +0200

    ANTIC-003 LoginFragment Layout [BL, NG]

---
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.swtug.anticovid.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.swtug.anticovid.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.fragment_main, null)
+    }
+}

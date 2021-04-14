d7cd5843b0ae1baae53c2e20f36f7ae40cd7a64e
Gabriel Nagler <naglergabriel.gn@gmail.com>
2021-04-14 18:03:47 +0200

    ANTIC-003 finished LoginFragment layout + created LoginViewModel [NG, BL]

--- AntiCovid/app/src/main/java/com/swtug/anticovid/login/LoginFragment.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/login/LoginFragment.kt
@@ -1,18 +1,28 @@
-package com.swtug.anticovid.main
+package com.swtug.anticovid.login
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.swtug.anticovid.R
 
 class LoginFragment : Fragment() {
+
+    private lateinit var loginViewModel: LoginViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return layoutInflater.inflate(R.layout.fragment_main, null)
+    ): View {
+        loginViewModel =
+            ViewModelProvider(this).get(LoginViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return root
     }
+
+
 }

bbddfc2d475613b06db1ad4d6d3e07b595f82ec3
Andreas Kohl <kohl@student.tugraz.at>
2021-04-21 15:03:37 +0200

    ANTIC-014 added test for QRCode creation

--- AntiCovid/app/src/main/java/com/swtug/anticovid/qrCode/QRCodeFragment.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/qrCode/QRCodeFragment.kt
@@ -1,16 +1,11 @@
 package com.swtug.anticovid.qrCode
 
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.swtug.anticovid.BaseFragment
 import com.swtug.anticovid.R
@@ -18,8 +13,10 @@ import com.swtug.anticovid.R
 
 class QRCodeFragment : BaseFragment() {
 
+    private val qrCodeGenerator = QRCodeGenerator()
+
     companion object {
-        private const val TAG = "qrTag"
+        const val TAG = "qrTag"
     }
 
     override fun onCreateView(
@@ -41,31 +38,18 @@ class QRCodeFragment : BaseFragment() {
                 "Test Date: 19.04.2021 \n" +
                 "Valid till: 21.04.2021\n" +
                 "Result: negative"   //TODO get Data from saved bitmap
-        val bitmap = generateQRCode(qrCodeContent)
+        val bitmap = try {
+            qrCodeGenerator.generateQRCode(qrCodeContent)
+        } catch (e: WriterException) {
+            e.printStackTrace()
+            null
+        }
+
         imvShowQRCode.setImageBitmap(bitmap)
 
         val textField = view.findViewById<TextView>(R.id.txvQRScreen)
 
         textField.text = qrCodeContent
-    }
-
-    private fun generateQRCode(qrCodeContent: String): Bitmap {
-
-        val width = 500
-        val height = 500
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val codeWriter = MultiFormatWriter()
-        try {
-            val bitMatrix = codeWriter.encode(qrCodeContent, BarcodeFormat.QR_CODE, width, height)
-            for (x in 0 until width) {
-                for (y in 0 until height) {
-                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
-                }
-            }
-        } catch (e: WriterException) {
-            Log.d(TAG, "generateQRCode: ${e.message}")
-        }
-        return bitmap
     }
 
 

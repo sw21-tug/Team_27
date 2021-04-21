3a00286e9fe7881fbc57f6c094de49ca62f77e3d
Bela Lemle <bela.lemle@student.tugraz.at>
2021-04-21 15:29:19 +0200

    ANTIC-001 registers front end is set up [BL, BC]

---
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.swtug.anticovid.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.swtug.anticovid.data.LoginDataSource
+import com.swtug.anticovid.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}

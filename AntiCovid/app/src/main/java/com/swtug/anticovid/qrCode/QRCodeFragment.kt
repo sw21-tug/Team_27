8519aff3b48aa66c08ededf1ebeca815fe15417c
Andreas Kohl <kohl@student.tugraz.at>
2021-04-21 14:24:13 +0200

    ANTIC-014 QRCode creation

--- AntiCovid/app/src/main/java/com/swtug/anticovid/qrCode/QRCodeFragment.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/qrCode/QRCodeFragment.kt
@@ -1,15 +1,72 @@
 package com.swtug.anticovid.qrCode
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
 import com.swtug.anticovid.BaseFragment
 import com.swtug.anticovid.R
 
-class QRCodeFragment: BaseFragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+class QRCodeFragment : BaseFragment() {
+
+    companion object {
+        private const val TAG = "qrTag"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return layoutInflater.inflate(R.layout.fragment_qr_code, null)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imvShowQRCode = view.findViewById<ImageView>(R.id.QRScreenQRCodeImg)
+
+        val qrCodeContent = "Informations \n" +
+                "\n" +
+                "Simon, Schrimpf \n" +
+                "Test Date: 19.04.2021 \n" +
+                "Valid till: 21.04.2021\n" +
+                "Result: negative"   //TODO get Data from saved bitmap
+        val bitmap = generateQRCode(qrCodeContent)
+        imvShowQRCode.setImageBitmap(bitmap)
+
+        val textField = view.findViewById<TextView>(R.id.txvQRScreen)
+
+        textField.text = qrCodeContent
+    }
+
+    private fun generateQRCode(qrCodeContent: String): Bitmap {
+
+        val width = 500
+        val height = 500
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val codeWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = codeWriter.encode(qrCodeContent, BarcodeFormat.QR_CODE, width, height)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        } catch (e: WriterException) {
+            Log.d(TAG, "generateQRCode: ${e.message}")
+        }
+        return bitmap
+    }
+
+
 }

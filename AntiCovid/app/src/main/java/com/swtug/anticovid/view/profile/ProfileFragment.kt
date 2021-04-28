c11c2900f3cae3ae413a1158d860679d4b21db2f
Balint Csordas <balint.csordas@student.tugraz.at>
2021-04-28 13:15:04 +0200

    ANTIC-004 Added logout button, on click it will redirect to login [SS, BC]

--- AntiCovid/app/src/main/java/com/swtug/anticovid/view/profile/ProfileFragment.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/view/profile/ProfileFragment.kt
@@ -4,11 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import com.swtug.anticovid.view.BaseFragment
 import com.swtug.anticovid.R
 
 class ProfileFragment: BaseFragment() {
+
+    private lateinit var btnlogout: MaterialButton
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return layoutInflater.inflate(R.layout.fragment_profile, null)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initFields(view)
+
+        btnlogout.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+        }
+    }
+    private fun initFields(view: View) {
+        btnlogout = view.findViewById(R.id.logoutbutton)
     }
 }

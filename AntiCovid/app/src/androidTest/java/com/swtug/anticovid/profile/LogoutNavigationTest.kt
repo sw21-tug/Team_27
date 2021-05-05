14a6f35615414e774f65309d69425c7c3067ff59
Gabriel Nagler <naglergabriel.gn@gmail.com>
2021-05-05 18:22:48 +0200

    ANTIC-026 fixed LogoutTest [NG, LB]

--- AntiCovid/app/src/androidTest/java/com/swtug/anticovid/profile/LogoutNavigationTest.kt
+++ AntiCovid/app/src/androidTest/java/com/swtug/anticovid/profile/LogoutNavigationTest.kt
@@ -2,6 +2,7 @@ package com.swtug.anticovid.profile
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.fragment.app.testing.withFragment
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
@@ -9,12 +10,18 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.swtug.anticovid.R
+import com.swtug.anticovid.TestUtils
+import com.swtug.anticovid.models.User
+import com.swtug.anticovid.repositories.PreferencesRepo
 import com.swtug.anticovid.view.profile.ProfileFragment
 import junit.framework.TestCase
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
 
 @RunWith(AndroidJUnit4::class)
@@ -23,6 +30,14 @@ class LogoutNavigationTest {
 
     @Before
     fun setup() {
+        PreferencesRepo.saveLocale(ApplicationProvider.getApplicationContext(), Locale.ENGLISH)
+
+        val user = User(1, "name", "surname", "email", "xyz 1", "1", "+3234324234", "password")
+
+        PreferencesRepo.saveUser(
+            InstrumentationRegistry.getInstrumentation().targetContext,
+            user
+        )
 
         navController = TestNavHostController(ApplicationProvider.getApplicationContext())
 
@@ -34,6 +49,10 @@ class LogoutNavigationTest {
             navController.setCurrentDestination(R.id.profileFragment)
             Navigation.setViewNavController(requireView(), navController)
         }
+    }
+    @After
+    fun tearDown() {
+        TestUtils.clearSharedPreferences(InstrumentationRegistry.getInstrumentation().targetContext)
     }
 
 

3848fab9c159e97e5e96e67f0a1a81e97939a3dc
Bela Lemle <bela.lemle@student.tugraz.at>
2021-05-05 15:56:48 +0200

    the load test works and test for the chinese translation [ST, LB]

--- AntiCovid/app/src/androidTest/java/com/swtug/anticovid/profile/LoadLanguageTest.kt
+++ AntiCovid/app/src/androidTest/java/com/swtug/anticovid/profile/LoadLanguageTest.kt
@@ -1,40 +1,37 @@
 package com.swtug.anticovid.profile
 
-import android.app.Activity
 import android.content.Context
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.fragment.app.testing.withFragment
-import androidx.navigation.Navigation
-import androidx.navigation.testing.TestNavHostController
+import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import com.swtug.anticovid.MainActivity
-import com.swtug.anticovid.R
 import com.swtug.anticovid.TestUtils
 import com.swtug.anticovid.repositories.PreferencesRepo
-import com.swtug.anticovid.view.profile.ProfileFragment
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.System.exit
 import java.util.*
+
 
 @RunWith(AndroidJUnit4::class)
 class LoadLanguageTest {
     @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity>
-            = ActivityScenarioRule(MainActivity::class.java)
+    var launchActivityRule = ActivityTestRule(
+        MainActivity::class.java, false, false
+    )
 
     @Before
     fun setup() {
 
         PreferencesRepo.saveLocale(ApplicationProvider.getApplicationContext(), Locale.SIMPLIFIED_CHINESE)
 
+        launchActivityRule.launchActivity(Intent(InstrumentationRegistry.getInstrumentation().targetContext, MainActivity::class.java))
 
     }
 
@@ -45,7 +42,13 @@ class LoadLanguageTest {
 
     @Test
     fun testLanguageChange() {
+
         assert((PreferencesRepo.getLocale(ApplicationProvider.getApplicationContext()))
                 == Locale.SIMPLIFIED_CHINESE)
+        Thread.sleep(3500)
+        Espresso.pressBackUnconditionally()
+
     }
+
+
 }

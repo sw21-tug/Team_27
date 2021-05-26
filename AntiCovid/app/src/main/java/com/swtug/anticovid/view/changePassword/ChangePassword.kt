3f972f6a2584ea9f1f89c43e15d90960717a83e5
Gabriel Nagler <naglergabriel.gn@gmail.com>
2021-05-26 02:02:39 +0200

    ANTIC-018 added functionality of changing password [NG, CB]

--- AntiCovid/app/src/main/java/com/swtug/anticovid/view/changePassword/ChangePassword.kt
+++ AntiCovid/app/src/main/java/com/swtug/anticovid/view/changePassword/ChangePassword.kt
@@ -1,7 +1,5 @@
 package com.swtug.anticovid.view.changePassword
 
-import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -10,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
+import androidx.navigation.fragment.findNavController
 import com.swtug.anticovid.R
 import com.swtug.anticovid.models.User
+import com.swtug.anticovid.repositories.FirebaseListener
+import com.swtug.anticovid.repositories.FirebaseRepo
 import com.swtug.anticovid.repositories.PreferencesRepo
 import com.swtug.anticovid.view.BaseFragment
 
@@ -37,7 +39,6 @@ class ChangePassword : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initFields(view)
-        bindFields()
         initListeners()
 
         user = PreferencesRepo.getUser(requireContext())!!
@@ -50,13 +51,8 @@ class ChangePassword : BaseFragment() {
         editTextCurrentPassword = view.findViewById(R.id.editTextOldPassword)
         editTextNewPassword = view.findViewById(R.id.editTextNewPassword)
         editTextRepeatPassword = view.findViewById(R.id.editTextRepeatPassword)
-    }
-
-    private fun bindFields() {
-
 
     }
-
 
     private fun initListeners() {
         editTextCurrentPassword.afterTextChanged {
@@ -65,34 +61,79 @@ class ChangePassword : BaseFragment() {
                 setTick(editTextCurrentPassword)
             else
                 setError(editTextCurrentPassword, getString(R.string.wrong_password))
-
-
         }
-
-
 
         editTextNewPassword.afterTextChanged {
             if (it.length < 8 || it == user.password)
                 setError(editTextNewPassword, getString(R.string.password_does_not_match_criteria))
             else
                 setTick(editTextNewPassword)
-
-
         }
 
         editTextRepeatPassword.afterTextChanged {
-            if (it == user.password && it == editTextNewPassword.text.toString())
+            if (editTextRepeatPassword.text.toString() == editTextNewPassword.text.toString())
                 setTick(editTextRepeatPassword)
             else
                 setError(editTextRepeatPassword, getString(R.string.passwords_do_not_match))
-
         }
 
         btnChangePassword.setOnClickListener {
-            //TODO
+            if ((editTextNewPassword.text.toString() == editTextRepeatPassword.text.toString()) && (editTextCurrentPassword.text.toString() == user.password)) {
+                val user = getNewUser(editTextNewPassword.text.toString())
 
-            //findNavController().navigate(R.id.action_profileFragment_to_loginFragment)
+                saveNewPasswordFirebase(user)
+            }else
+                Toast.makeText(
+                    requireContext(),
+                    requireContext().getString(R.string.error_fill_out_filds),
+                    Toast.LENGTH_LONG
+                ).show()
         }
+    }
+
+
+    private fun saveNewPasswordFirebase(user: User) {
+        FirebaseRepo.updateUser(user, object : FirebaseListener {
+            override fun onSuccess(user: User?) {
+                btnChangePassword.isEnabled = true
+
+                user?.let { PreferencesRepo.saveUser(requireContext(), it) }
+                findNavController().navigate(R.id.action_changePasswordFragment_to_profileFragment)
+
+                Toast.makeText(
+                    requireContext(),
+                    requireContext().getString(R.string.changes_saved),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onStart() {
+                btnChangePassword.isEnabled = false
+            }
+
+            override fun onFailure() {
+                btnChangePassword.isEnabled = true
+
+                Toast.makeText(
+                    requireContext(),
+                    requireContext().getString(R.string.error_firebase_communication),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+    }
+
+    private fun getNewUser(password: String): User {
+        return User(
+            user.id,
+            user.name,
+            user.surname,
+            user.email,
+            user.address,
+            user.secid,
+            user.phonenumber,
+            password
+        )
     }
 
     private fun setError(editText: EditText, error: String) {
@@ -107,6 +148,7 @@ class ChangePassword : BaseFragment() {
     }
 
     private fun setTick(editText: EditText) {
+        //TODO: fix this function...currently it's not working
         val icon = ResourcesCompat.getDrawable(
             resources,
             R.drawable.ic_baseline_check_circle_24,
@@ -135,11 +177,6 @@ class ChangePassword : BaseFragment() {
             }
         })
     }
-
-    private fun EditText.setDrawables(start: Drawable?, top: Drawable?, end: Drawable?, buttom: Drawable?) {
-        setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, buttom)
-    }
-
 
 }
 

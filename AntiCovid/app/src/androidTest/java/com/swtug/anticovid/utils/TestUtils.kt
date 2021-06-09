8c0837a80e34e11b4130b44b8a2b635d788bed90
Lukas Brugger <lukas.brugger404@gmail.com>
2021-06-09 16:41:22 +0200

    ANTIC-051 Refactor Login Error Messages [BL, ST]
    Tests

--- AntiCovid/app/src/androidTest/java/com/swtug/anticovid/utils/TestUtils.kt
+++ AntiCovid/app/src/androidTest/java/com/swtug/anticovid/utils/TestUtils.kt
@@ -2,13 +2,15 @@ package com.swtug.anticovid.utils
 
 import android.content.Context
 import android.view.View
-import androidx.test.espresso.PerformException
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
+import android.view.WindowManager
+import androidx.test.espresso.*
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.google.android.material.tabs.TabLayout
 import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+
 
 object TestUtils {
 
@@ -22,13 +24,22 @@ object TestUtils {
     fun getResoureceString(context: Context, id: Int): String {
         return context.getString(id)
     }
+
+
+
 }
+
+
+
 
 fun selectTabAtPosition(tabIndex: Int): ViewAction {
     return object : ViewAction {
         override fun getDescription() = "with tab at index $tabIndex"
 
-        override fun getConstraints() = allOf(isDisplayed(), isAssignableFrom(TabLayout::class.java))
+        override fun getConstraints() = allOf(
+            isDisplayed(),
+            isAssignableFrom(TabLayout::class.java)
+        )
 
         override fun perform(uiController: UiController, view: View) {
             val tabLayout = view as TabLayout
